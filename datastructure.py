cd709a46a44932fe713438fccbe15f5e3d8ceace
Israel <bamideleisrael04@gmail.com>
2024-08-19 23:56:29 +0100

    Corrected path of data_s1 and s2 to be read

--- datastructure.py
+++ datastructure.py
@@ -1,8 +1,8 @@
 import scipy.io
 
 # Load the .mat files
-data_s1 = scipy.io.loadmat('S1.mat')
-data_s2 = scipy.io.loadmat('S2.mat')
+data_s1 = scipy.io.loadmat('dataset/S1.mat')
+data_s2 = scipy.io.loadmat('dataset/S2.mat')
 
 # Print keys to understand the structure
 print("Keys in S1.mat:", data_s1.keys())
