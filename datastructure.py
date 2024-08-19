f4177fc73805e8b6a5352f9b804ce031b3363767
Israel <bamideleisrael04@gmail.com>
2024-08-19 11:19:49 +0100

    Added the requirements.txt, .venv and datastructure.py for assessing the dataset

---
+++ datastructure.py
@@ -0,0 +1,9 @@
+import scipy.io
+
+# Load the .mat files
+data_s1 = scipy.io.loadmat('S1.mat')
+data_s2 = scipy.io.loadmat('S2.mat')
+
+# Print keys to understand the structure
+print("Keys in S1.mat:", data_s1.keys())
+print("Keys in S2.mat:", data_s2.keys())
