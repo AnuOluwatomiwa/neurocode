54a0ce34a4ec957c73ca906566de879e8950f29b
Israel <bamideleisrael04@gmail.com>
2024-08-20 22:52:56 +0100

    Inspect the info key

--- datastructure.py
+++ datastructure.py
@@ -7,3 +7,10 @@ data_s2 = scipy.io.loadmat('dataset/S2.mat')
 # Print keys to understand the structure
 print("Keys in S1.mat:", data_s1.keys())
 print("Keys in S2.mat:", data_s2.keys())
+
+#Inspect the 'Info' key
+info_s1 = data_s1['Info']
+info_s2 = data_s2['Info']
+
+print("Info for S1.mat:\n", info_s1)
+print("\nInfo for S2.mat:\n",info_s2)
