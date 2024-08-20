5892f358c31bd8263f0d48303c6d0d8442c8206c
Israel <bamideleisrael04@gmail.com>
2024-08-20 22:53:49 +0100

    Explore the 'training_data' and 'test_data' Keys

--- datastructure.py
+++ datastructure.py
@@ -14,3 +14,7 @@ info_s2 = data_s2['Info']
 
 print("Info for S1.mat:\n", info_s1)
 print("\nInfo for S2.mat:\n",info_s2)
+
+# Check the structure of 'training_data' and 'test_data'
+training_data_s1 = data_s1['training_data']
+test_data_s1 = data_s1['test_data']
